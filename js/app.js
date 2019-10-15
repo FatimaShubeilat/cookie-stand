d2390a67b52a93f03967575cb25f82f9b836349b
fatfooot <fshubeilat@gmail.com>
2019-10-15 22:10:24 +0300

    I modified the table, appears that I had misunderstanding the table raw and table cell this is why I had a dublicated rows

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
   
-var hours = ['' ,'6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 
 function States(location, min, max, avg){
@@ -28,56 +28,106 @@ States.prototype.amountOfCookiesPerHourFunction = function(){
 
       this.cookiesArray.push(amountOfCookiesCeil);
       this.dailyCookieSales += amountOfCookiesCeil;
+     
     }
-}
-
-States.prototype.outpuHTML = function() {
-  var container = document.getElementById('ShopsData');
-  var article = document.createElement('article');
-  container.appendChild(article);
-// do table
-
-
-// header row
-var headerRow = document.createElement('tr');
-article.appendChild(headerRow);
-
-for(var i=0 ; i<hours.length; i++){
-var statesHeader = document.createElement('th');
-headerRow.appendChild(statesHeader);
-statesHeader.textContent = hours[i];
-}
-
-// Seattle Row
-var dataRow = document.createElement('tr');
-article.appendChild(dataRow);
-
-var cookieData = document.createElement('th');
-dataRow.appendChild(cookieData);
-cookieData.textContent = this.location;
-
-// Seattle Row
-var dataRow = document.createElement('tr');
-article.appendChild(dataRow);
-
-var cookieData = document.createElement('th');
-dataRow.appendChild(cookieData);
-cookieData.textContent = 'Totals';
-
-
-
+    // console.log(this.dailyCookieSales);
 }
 
 
-var seattle = new States('Seattle', 23, 65, 6.3);
-var tokyo = new States('Tokyo', 20, 50, 1.2 );
-var dubai = new States('Dubai', 11,38,3.7);
-var paris = new States('Paris', 20 ,38, 2.3);
-var lima = new States('Lima', 2,16,4.6);
+States.prototype.outputToHTML = function(table) {
+  var tr = document.createElement('tr');
+  table.appendChild(tr);
 
+  var td = document.createElement('td');
+  tr.appendChild(td);
+  td.textContent = this.location;
 
-var states = [seattle, tokyo, dubai,paris,lima];
-for (var i = 0; i < states.length; i++) {
-  states[i].outpuHTML();
-console.log(states[i]);
+  for(var i=0; i<hours.length ; i++) {
+    td = document.createElement('td');
+    tr.appendChild(td);
+    td.textContent = this.cookiesArray[i];
+  }
+
+  td = document.createElement('td');
+  tr.appendChild(td);
+  td.textContent = this.dailyCookieSales;
+  console.log(this.dailyCookieSales);
 }
+
+// Header row function
+function outputHRow(table) {
+  var tr = document.createElement('tr');
+  table.appendChild(tr);
+  var th = document.createElement('th');
+
+  tr.appendChild(th);
+
+  for(var i=0; i <hours.length; i++){
+    th = document.createElement('th');
+    tr.appendChild(th);
+    th.textContent = hours[i];
+  }
+
+  th = document.createElement('th');
+  tr.appendChild(th);
+  th.textContent = 'Daily Location Total'
+}
+    
+
+// foorter function
+
+function outputFRow(table) {
+  var tr = document.createElement('tr');
+  table.appendChild(tr);
+  var td = document.createElement('td');
+  tr.appendChild(td);
+  td.textContent = 'Totals';
+
+  var totalCount = 0;
+  for (var j=0; j< hours.length ; j++) {
+    td = document.createElement('td');
+    tr.appendChild(td);
+
+    var countSum = 0;
+    for (var i=0; i < shops.length ; i ++) {
+      var shop = shops[i];
+      countSum += shop.cookiesArray[j];
+    }
+    td.textContent = countSum;
+    totalCount += countSum;
+
+  }
+
+  td = document.createElement('td');
+  tr.appendChild(td);
+  td.textContent = totalCount;
+}
+
+
+var seattle = new States('Seattle',23, 65, 6.5);
+var dubai = new States('Dubai',11, 38, 3.7);
+var tokyo = new States('Tokyo',3, 24, 1.2);
+var paris = new States('Paris',20, 38, 2.3);
+var lima = new States('Lima',2, 16, 4.6);
+
+
+var shops = [seattle, dubai, tokyo, paris, lima]; // add more shops when ready
+
+
+var container = document.getElementById('content-area');
+
+var table = document.createElement('table');
+
+container.appendChild(table);
+
+outputHRow(table);
+
+for (var i = 0; i < shops.length; i++) {
+  
+  var shop = shops[i];
+
+  shop.outputToHTML(table);
+}
+
+outputFRow(table);
+
